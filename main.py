6b90b24b258c871744b938aecfd2330ca134b41b
liufran1 <16155488+liufran1@users.noreply.github.com>
2023-05-31 14:00:18 +0000

    Cleanup to remove extra webpages and error handling

--- main.py
+++ main.py
@@ -1,41 +1,29 @@
-import random, string
-
 import restuarantsummarize
+import re
+import random
 
-from flask import Flask, render_template, request
+from flask import Flask, request
 app = Flask(  # Create a flask app
 	__name__,
-	template_folder='templates',  # Name of html file folder
-	static_folder='static'  # Name of directory for static files
 )
 
-ok_chars = string.ascii_letters + string.digits
-@app.route('/')  # What happens when the user visits the site
-def base_page():
-	random_num = random.randint(1, 100000)  # Sets the random number
-	return render_template(
-		'base.html',  # Template file path, starting from the templates folder. 
-		random_number=random_num  # Sets the variable random_number in the template
-	)
-@app.route('/2')
-def page_2():
-	rand_ammnt = random.randint(10, 100)
-	random_str = ''.join(random.choice(ok_chars) for a in range(rand_ammnt))
-	return render_template('site_2.html', random_str=random_str)
 
-# @app.route('/reviews')
 @app.route('/reviews', methods=['GET'])
 def get_summary():
+  pattern = re.compile("^.*yelp.com\/biz.*$")
+  
   input_url = request.args.get('input_url')
   if input_url is None:
-    return 'Missing input_url parameter', 400
+    return 'Input a valid url', 400
+  if not pattern.match(input_url):
+    return 'Input a yelp website', 400
+  
+
   
   cleansed_reviews = restuarantsummarize.get_top_cleanreviews(input_url)
   prompt = restuarantsummarize.format_prompt(cleansed_reviews)
   
   result = restuarantsummarize.get_completion(prompt)
-  # result = input_url
-  # return result, 200
   return f"review summary for {input_url}: {result}", 200
 
 
