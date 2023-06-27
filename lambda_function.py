f7686ee2159de5f609c3820c7a0766edc5c450dc
Franklin Liu <16155488+liufran1@users.noreply.github.com>
2023-06-26 21:06:27 -0400

    Update lambda_function.py to add try-catch blocks

--- lambda_function.py
+++ lambda_function.py
@@ -20,32 +20,28 @@ def lambda_handler(event, context):
                 'statusCode': 200,
                 'body': json.dumps('Input a valid url')
             }
-
-    if pattern.match(input_url):
-      cleansed_reviews = restuarantsummarize.get_top_yelp_cleanreviews(input_url)
-    else:  
-      cleansed_reviews = restuarantsummarize.get_reviews(input_url)
-    
-    
-    
-    if not cleansed_reviews:
+    try: 
+        if pattern.match(input_url):
+            cleansed_reviews = restuarantsummarize.get_top_yelp_cleanreviews(restuarantsummarize.clean_url(input_url))
+        else:  
+            cleansed_reviews = restuarantsummarize.get_reviews(restuarantsummarize.clean_url(input_url))
+    except:
         return {
                 'statusCode': 400,
-                'body': json.dumps('Error pulling reviews from site')
+                'body': json.dumps('Whoops there was a problem getting reviews from site')
             }
 
     
     prompt = restuarantsummarize.format_prompt(cleansed_reviews)
-
-    result = restuarantsummarize.get_completion(prompt)
-    if result:
+    
+    try:
+        result = restuarantsummarize.get_completion(prompt)
         return {
                 'statusCode': 200,
                 'body': json.dumps(f"review summary for {input_url}: {result}")
             }
-    else:
+    except:
         return {
                 'statusCode': 400,
-                'body': json.dumps('Error getting summary')
+                'body': json.dumps('Looks like there was a problem getting the computer to summarize these reviews')
             }
-
